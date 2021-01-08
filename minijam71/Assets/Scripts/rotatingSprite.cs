4dcaa819ada7f12f6a1e6ece249fd64a9cb7a737
Jeanne <jeanne.hecquard@gmail.com>
2021-01-09 00:42:51 +0100

    Scene rotating sprite

---
+++ minijam71/Assets/Scripts/rotatingSprite.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotatingSprite : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public Camera cam;
+     
+    void Update () {
+         transform.rotation = Quaternion.LookRotation(-cam.transform.forward);
+     }
+}
