d5211c7dd174f53c2fa2e3e4ccb4f98e93fa9986
Jeanne <jeanne.hecquard@gmail.com>
2021-01-09 04:46:56 +0100

    Ajouts assets randoms

---
+++ minijam71/Assets/Scripts/moveScene.cs
@@ -0,0 +1,39 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class moveScene : MonoBehaviour
+{
+    public string LevelToLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        //Debug.Log("Started");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Waiting for collision");
+    }
+
+    void OnMouseDown()
+    {
+        //Debug.Log("Collision detected");
+
+        if(gameObject.tag == "InteractObject"|| gameObject.tag == "InteractObject2")
+        {
+            SceneManager.LoadScene(LevelToLoad);
+        }
+        else
+        {
+            Debug.Log("Not an object you can interact with");
+        }
+    }
+}
---
+++ minijam71/Assets/Scripts/switchCameras.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switchCameras : MonoBehaviour
+{
+
+    public Camera camera1;
+    public Camera camera2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera1.enabled = true;
+        camera2.enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown() {
+
+        camera2.enabled = !(camera2.enabled);
+        camera1.enabled = !(camera1.enabled);
+    }
+}
