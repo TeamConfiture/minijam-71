d6f46e3b00b76a3f60e31b4f583b40736e5f6248
Solok the Baguette Boy <solok-sardine@hotmail.com>
2021-01-10 18:02:36 +0100

    Début dernière énigme
    Début dernière énigme

---
+++ minijam71/Assets/Scripts/LastTrialScripts/LastTrial.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LastTrial : MonoBehaviour
+
+    
+{
+    [SerializeField] GameObject inputForAnswer;
+    [SerializeField] private TMP_Text tmp;
+    [SerializeField] private InputField inpF;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Loading scene : Final Trial");
+        inputForAnswer.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Current Scene : Final Trial");
+    }
+
+    public void HandleClick()
+    {
+        Debug.Log("Clicked on this red thing");
+        LastTrialEnigma();
+        
+    }
+
+    void LastTrialEnigma()
+    {
+
+        if (inputForAnswer.activeSelf == false)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            inputForAnswer.SetActive(true);
+            tmp.SetText("So you've traveled from the first room to this final, third room ? \n1 + 1 = _ _ \nWoah mommy, it's so shiny !");
+            inpF.ActivateInputField();
+            inpF.Select();
+
+        }
+
+        else 
+        {
+
+            inputForAnswer.SetActive(false);
+            tmp.SetText("TIME TO PERISH");
+            inpF.DeactivateInputField();
+            inpF.Select();
+
+        }
+
+        string answer = "";
+        
+        // get the player's answer and assign it to answer
+
+        if (answer == "423")
+        {
+            // open door
+            Debug.Log("GG WP, you saved the rabbits");
+        }
+
+        else
+        {
+            Debug.Log("GG WP, you saved the rabbits");
+        }
+
+
+    }
+}
---
+++ minijam71/Assets/Scripts/LastTrialScripts/TextHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class TextHandler : MonoBehaviour
+
+{
+    [SerializeField] private TMP_Text tmp;
+    [SerializeField] private InputField inpF;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //tmp.SetText("So you've traveled from the first room to this final, third room ? \n1 + 1 = _ _ \nWoah mommy, it's so shiny !");
+    }
+}
