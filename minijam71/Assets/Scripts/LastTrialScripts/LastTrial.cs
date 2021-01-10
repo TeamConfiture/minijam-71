4491c7f289e6e69df1a95a56c5a32adff12db7df
unknown <shinigamidaikou18@gmail.com>
2021-01-10 22:52:41 +0100

    Trial Textuelle
    -Menu textuel
    -Pause pendant le menu
    -fonction de dépause

--- minijam71/Assets/Scripts/LastTrialScripts/LastTrial.cs
+++ minijam71/Assets/Scripts/LastTrialScripts/LastTrial.cs
@@ -3,26 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class LastTrial : MonoBehaviour
 
     
 {
-    [SerializeField] GameObject inputForAnswer;
-    [SerializeField] private TMP_Text tmp;
-    [SerializeField] private InputField inpF;
+    //[SerializeField] GameObject inputForAnswer;
+
+    [SerializeField] GameObject selectManag;
+
+    [SerializeField] GameObject exit;
+    [SerializeField] FirstPersonController player;
+    [SerializeField] TMP_Text tmp;
+    [SerializeField] public InputField inpF;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Loading scene : Final Trial");
-        inputForAnswer.SetActive(false);
+       // inputForAnswer.SetActive(false);
+        exit.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Current Scene : Final Trial");
+        //Debug.Log("Current Scene : Final Trial");
     }
 
     public void HandleClick()
@@ -32,24 +39,40 @@ public class LastTrial : MonoBehaviour
         
     }
 
+    public void Pause(){
+        Time.timeScale =0;
+        player.enabled = !player.enabled;
+        selectManag.SetActive(false);
+        exit.SetActive(true);
+    }
+
+    public void Unpause(){
+        Time.timeScale =1.0f;
+        player.enabled = !player.enabled;
+        selectManag.SetActive(true);
+        exit.SetActive(false);
+    }
+
     void LastTrialEnigma()
     {
 
-        if (inputForAnswer.activeSelf == false)
+        if (exit.activeSelf == false)
         {
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
-            inputForAnswer.SetActive(true);
+            //inputForAnswer.SetActive(true);
             tmp.SetText("So you've traveled from the first room to this final, third room ? \n1 + 1 = _ _ \nWoah mommy, it's so shiny !");
             inpF.ActivateInputField();
             inpF.Select();
+            Pause();
+            
 
         }
 
         else 
         {
 
-            inputForAnswer.SetActive(false);
+            //inputForAnswer.SetActive(false);
             tmp.SetText("TIME TO PERISH");
             inpF.DeactivateInputField();
             inpF.Select();
