a839f0534433fd476a57e8917fc1786e8a84387d
Kimi <tran_kim-anh97@hotmail.fr>
2021-01-10 15:59:13 +0100

    add main menu

---
+++ minijam71/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject CreditsMenu;
+
+    public AudioSource soundEffect;
+    // Start is called before the first frame update
+    public void Start()
+    {
+
+    }
+
+    public void PlayNowButton(string scene)
+    {
+        // UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
+        soundEffect.Play();
+    }
+    public void CreditsButton()
+    {
+        CreditsMenu.SetActive(true);
+        MainMenu.SetActive(false);
+        soundEffect.Play();
+    }
+    public void MainMenuButton()
+    {
+        MainMenu.SetActive(true);
+        CreditsMenu.SetActive(false);
+    }
+    public void BackButton()
+    {
+        MainMenu.SetActive(true);
+        CreditsMenu.SetActive(false);
+        soundEffect.Play();
+    }
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
