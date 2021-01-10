c712ae5c1dd158cb3002c226da034802e0d73fb8
unknown <shinigamidaikou18@gmail.com>
2021-01-11 00:44:45 +0100

    ""Pause""
    -press esc to enter pause (also final trial)

--- minijam71/Assets/Scripts/GameManager.cs
+++ minijam71/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 ///<summary>This object is singleton. It is Suposed to make sure that every data are correctly transmitted between all scenes
 ///and that the representations are correctly handled according to player progression</summary>
@@ -10,6 +11,12 @@ public class GameManager : MonoBehaviour
     private static GameManager instance;
     ///Contain all nodes of story to keep track of player progression
     [SerializeField] private Key[] KeysStatus;
+    [SerializeField] GameObject selectManag;
+
+    [SerializeField] GameObject exit;
+    [SerializeField] FirstPersonController player;
+
+    private bool isPaused = false;
     #endregion
 
     private void Awake() {
@@ -26,6 +33,28 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape) && !isPaused){
+            PauseMain();
+            isPaused = !isPaused;
+        }
+
+        if(Input.GetMouseButtonDown(0) && isPaused){
+            UnpauseMain();
+            isPaused = !isPaused;
+        }
+    }
+
+    public void PauseMain(){
+        Time.timeScale =0;
+        player.enabled = !player.enabled;
+        selectManag.SetActive(false);
+        exit.SetActive(true);
+    }
+
+    public void UnpauseMain(){
+        Time.timeScale =1.0f;
+        player.enabled = !player.enabled;
+        selectManag.SetActive(true);
+        exit.SetActive(false);
     }
 }
